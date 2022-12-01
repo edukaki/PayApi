352996c416e7ba013a3309cf9ba2f489b68308b8
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-01 17:07:09 +0900

    feat: create title

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames"
+import style from './Title.module.scss'
+
+type TitleProps = {
+    children: string
+    color?: 'blue' | 'white'
+    type: 'main' | 'section' | 'common'
+}
+
+const Title = ({ children, type, color }: TitleProps) => {
+    if (type === 'main') {
+        return (
+            <h1 className={(color === 'blue' ? 
+                classNames(style.general,style.main,style['main--blue']) :
+                classNames(style.general,style.main,style['main--white'])
+            )}>{children}</h1>
+        )
+    } else if(type === 'section'){
+        return (
+            <h2 className={(color === 'blue' ? 
+            classNames(style.general,style.section,style['section--blue']) :
+            classNames(style.general,style.section,style['section--white'])
+        )}>{children}</h2>
+        )
+    }else{
+        return(
+            <h3 className={classNames(style.general,style.common)}>{children}</h3>
+        )
+    }
+}
+
+export default Title
