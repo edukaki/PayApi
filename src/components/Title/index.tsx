e62e4f07dfb72b2157a779c951387c150d585268
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-04 11:25:37 +0900

    feat(title): change call of props

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -3,21 +3,21 @@ import style from './Title.module.scss'
 
 type TitleProps = {
     children: string
-    color?: 'blue' | 'white'
+    textColor?: 'blue' | 'white'
     type: 'main' | 'section' | 'common'
 }
 
-const Title = ({ children, type, color }: TitleProps) => {
+const Title = ({ children, type, textColor }: TitleProps) => {
     if (type === 'main') {
         return (
-            <h1 className={(color === 'blue' ? 
+            <h1 className={(textColor === 'blue' ? 
                 classNames(style.general,style.main,style['main--blue']) :
                 classNames(style.general,style.main,style['main--white'])
             )}>{children}</h1>
         )
     } else if(type === 'section'){
         return (
-            <h2 className={(color === 'blue' ? 
+            <h2 className={(textColor === 'blue' ? 
             classNames(style.general,style.section,style['section--blue']) :
             classNames(style.general,style.section,style['section--white'])
         )}>{children}</h2>
