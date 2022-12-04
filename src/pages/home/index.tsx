d1091c0605fffd24430f5f4feb824829b718a9fd
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-04 20:51:41 +0900

    feat: add Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import AboutCard from '../../components/AboutCard'
+import Facilities from '../../components/Facilities'
+import HomeHeader from '../../components/HomeHeader'
+
+const Home = () => {
+  return (
+    <main>
+      <HomeHeader />
+      <AboutCard />
+      <Facilities />
+    </main>
+  )
+}
+
+export default Home
