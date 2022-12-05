a2d5576da1b2dda546f796681558fcd8f639f655
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-06 06:46:21 +0900

    feat: add home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,11 @@
 import AboutCard from '../../components/AboutCard'
 import Facilities from '../../components/Facilities'
 import HomeHeader from '../../components/HomeHeader'
+import Style from '../styles/Pages.module.scss'
 
 const Home = () => {
   return (
-    <main>
+    <main className={Style['main__background--home']}>
       <HomeHeader />
       <AboutCard />
       <Facilities />
