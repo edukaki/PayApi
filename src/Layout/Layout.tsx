821dd211bee2f6cf41b2ed374927412e9f3ad74c
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-07 14:58:21 +0900

    feat: add footer to the layout

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar"
 import { Outlet } from 'react-router-dom'
+import Footer from "../components/Footer"
 
 const Layout = () => {
     return (
         <>
             <Navbar />
             <Outlet />
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
