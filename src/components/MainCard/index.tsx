3d54959fb2a76d05f52bd4be73569ae15f780200
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-08 18:25:56 +0900

    style(mainCard): refactor to keep the code clean and  make the image conditional

--- src/components/MainCard/index.tsx
+++ src/components/MainCard/index.tsx
@@ -3,22 +3,30 @@ import Paragraph from "../Paragraph"
 import Title from "../Title"
 import Style from './MainCard.module.scss'
 
-const MainCard = ({facilitiesImage,facilitiesImageAlt, imageType,titleText,paragraphText}:
-    {facilitiesImage?:string, facilitiesImageAlt?: string , imageType?: string, titleText: string, paragraphText: string}) => {
+const MainCard = ({ facilitiesImage, facilitiesImageAlt, imageType, titleText, paragraphText }:
+    { facilitiesImage?: string | undefined, facilitiesImageAlt?: string, imageType?: string, titleText: string, paragraphText: string }) => {
     return (
         <section className={Style.card}>
-            <img className={imageType === 'image' ? 
-            classNames(Style['image--normal']) :
-            classNames(Style['image--icon'])} 
-            src={facilitiesImage} alt={facilitiesImageAlt} />
+            {
+                facilitiesImage ?
+                    insertFacilitiesImage(imageType, facilitiesImage, facilitiesImageAlt)
+                    : ''
+            }
             <div className={Style.card__text}>
-            <Title type='section' textColor='blue'>{titleText}</Title>
+                <Title type='section' textColor='blue'>{titleText}</Title>
                 <Paragraph textColor='blue'>
                     {paragraphText}
                 </Paragraph>
             </div>
         </section>
     )
+}
+
+function insertFacilitiesImage(imageType: string | undefined, facilitiesImage: string, facilitiesImageAlt: string | undefined) {
+    return <img className={imageType === 'image' ?
+        classNames(Style['image--normal']) :
+        classNames(Style['image--icon'])}
+        src={facilitiesImage} alt={facilitiesImageAlt} />
 }
 
 export default MainCard
