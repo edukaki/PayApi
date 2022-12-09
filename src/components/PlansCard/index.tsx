d1641e82233cd9f88f4e5debe5a011ec28d5f292
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-09 17:31:09 +0900

    feat: add plansCard

---
+++ src/components/PlansCard/index.tsx
@@ -0,0 +1,38 @@
+import Title from '../Title'
+import {ReactComponent as Check} from '../../assets/img/shared/desktop/icon-check.svg'
+import { v4 as uuid } from 'uuid'
+import Paragraph from '../Paragraph'
+import BtnGeneral from '../BtnGeneral'
+import Style from './PlansCard.module.scss'
+
+type PlanProps ={
+    title: string,
+    price: string
+    benefits: Array<BenefitsProps>
+}
+
+type BenefitsProps ={
+    title:string,
+    include:boolean
+}
+
+const PlansCard = (Plan:PlanProps) => {
+    return (
+        <div>
+            <Title type='common'>{Plan.title}</Title>
+            <span>{Plan.price}</span>
+            <div className={Style.card}>
+                {Plan.benefits.map(benefit => (
+                    <div className={Style.card__check__text} key={uuid()}>
+                        {benefit.include ? 
+                        <Check stroke="#BA4270" /> : <Check stroke='none' />}
+                        <Paragraph textColor='blue'>{benefit.title}</Paragraph>
+                    </div>
+                ))}
+            </div>
+            <BtnGeneral btnType='secondary' backgroundTheme='light'>Request Access</BtnGeneral> 
+        </div>
+    )
+}
+
+export default PlansCard
