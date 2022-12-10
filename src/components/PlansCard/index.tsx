8d0555214dc20b480cece5e7472076d4713cea96
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-10 18:53:14 +0900

    feat: format plansCard

--- src/components/PlansCard/index.tsx
+++ src/components/PlansCard/index.tsx
@@ -1,36 +1,38 @@
 import Title from '../Title'
-import {ReactComponent as Check} from '../../assets/img/shared/desktop/icon-check.svg'
+import { ReactComponent as Check } from '../../assets/img/shared/desktop/icon-check.svg'
 import { v4 as uuid } from 'uuid'
 import Paragraph from '../Paragraph'
 import BtnGeneral from '../BtnGeneral'
 import Style from './PlansCard.module.scss'
 
-type PlanProps ={
+type PlanProps = {
     title: string,
     price: string
     benefits: Array<BenefitsProps>
 }
 
-type BenefitsProps ={
-    title:string,
-    include:boolean
+type BenefitsProps = {
+    title: string,
+    include: boolean
 }
 
-const PlansCard = (Plan:PlanProps) => {
+const PlansCard = (Plan: PlanProps) => {
     return (
-        <div>
+        <div className={Style.plan}>
             <Title type='common'>{Plan.title}</Title>
             <span>{Plan.price}</span>
             <div className={Style.card}>
-                {Plan.benefits.map(benefit => (
-                    <div className={Style.card__check__text} key={uuid()}>
-                        {benefit.include ? 
-                        <Check stroke="#BA4270" /> : <Check stroke='none' />}
-                        <Paragraph textColor='blue'>{benefit.title}</Paragraph>
-                    </div>
-                ))}
+                <div className={Style.card__benefits}>
+                    {Plan.benefits.map(benefit => (
+                        <div className={Style.card__check__text} key={uuid()}>
+                            {benefit.include ?
+                                <Check stroke="#BA4270" /> : <Check stroke='none' />}
+                            <Paragraph textColor='blue'>{benefit.title}</Paragraph>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <BtnGeneral btnType='secondary' backgroundTheme='light'>Request Access</BtnGeneral> 
+            <BtnGeneral btnType='secondary' backgroundTheme='light'>Request Access</BtnGeneral>
         </div>
     )
 }
