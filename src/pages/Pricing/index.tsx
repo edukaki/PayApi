4c941951019bad936ebe142066d446f8af1c646a
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-19 00:08:18 +0900

    feat(pricing): add description

--- src/pages/Pricing/index.tsx
+++ src/pages/Pricing/index.tsx
@@ -3,124 +3,131 @@ import CallAction from '../../components/CallAction'
 import PlansCard from '../../components/PlansCard'
 import Title from '../../components/Title'
 import Style from '../styles/Pages.module.scss'
+import StylePricing from './Pricing.module.scss'
 
 const Pricing = () => {
     return (
         <main className={Style['main__background--generic']}>
             <div className={classNames(Style.gallery, Style.gallery__secondary)}>
                 <Title type='main' textColor='blue'>Pricing</Title>
-                <PlansCard
-                    title='Free Plan'
-                    price='$0.00'
-                    benefits={
-                        [
-                            {
-                                title: 'Transaction',
-                                include: true
-                            },
-                            {
-                                title: 'Auth',
-                                include: true
-                            },
-                            {
-                                title: 'Identity',
-                                include: true
-                            },
-                            {
-                                title: 'Investments',
-                                include: false
-                            },
-                            {
-                                title: 'Assets',
-                                include: false
-                            },
-                            {
-                                title: 'Liabilities',
-                                include: false
-                            },
-                            {
-                                title: 'Income',
-                                include: false
-                            }
-                        ]
-                    }
-                />
+                <div className={StylePricing.pricing}>
 
-<PlansCard
-                    title='Basic Plan'
-                    price='$249.00'
-                    benefits={
-                        [
-                            {
-                                title: 'Transaction',
-                                include: true
-                            },
-                            {
-                                title: 'Auth',
-                                include: true
-                            },
-                            {
-                                title: 'Identity',
-                                include: true
-                            },
-                            {
-                                title: 'Investments',
-                                include: true
-                            },
-                            {
-                                title: 'Assets',
-                                include: true
-                            },
-                            {
-                                title: 'Liabilities',
-                                include: false
-                            },
-                            {
-                                title: 'Income',
-                                include: false
-                            }
-                        ]
-                    }
-                />
+                    <PlansCard
+                        title='Free Plan'
+                        description='Build and test using our core set of products with up to 100 API requests '
+                        price='$0.00'
+                        benefits={
+                            [
+                                {
+                                    title: 'Transaction',
+                                    include: true
+                                },
+                                {
+                                    title: 'Auth',
+                                    include: true
+                                },
+                                {
+                                    title: 'Identity',
+                                    include: true
+                                },
+                                {
+                                    title: 'Investments',
+                                    include: false
+                                },
+                                {
+                                    title: 'Assets',
+                                    include: false
+                                },
+                                {
+                                    title: 'Liabilities',
+                                    include: false
+                                },
+                                {
+                                    title: 'Income',
+                                    include: false
+                                }
+                            ]
+                        }
+                    />
 
-<PlansCard
-                    title='Premium Plan'
-                    price='$499.00'
-                    benefits={
-                        [
-                            {
-                                title: 'Transaction',
-                                include: true
-                            },
-                            {
-                                title: 'Auth',
-                                include: true
-                            },
-                            {
-                                title: 'Identity',
-                                include: true
-                            },
-                            {
-                                title: 'Investments',
-                                include: true
-                            },
-                            {
-                                title: 'Assets',
-                                include: true
-                            },
-                            {
-                                title: 'Liabilities',
-                                include: true
-                            },
-                            {
-                                title: 'Income',
-                                include: true
-                            }
-                        ]
-                    }
-                />
+                    <PlansCard
+                        title='Basic Plan'
+                        description='Launch your project with unlimited requests and no contractual minimums '
+                        price='$249.00'
+                        benefits={
+                            [
+                                {
+                                    title: 'Transaction',
+                                    include: true
+                                },
+                                {
+                                    title: 'Auth',
+                                    include: true
+                                },
+                                {
+                                    title: 'Identity',
+                                    include: true
+                                },
+                                {
+                                    title: 'Investments',
+                                    include: true
+                                },
+                                {
+                                    title: 'Assets',
+                                    include: true
+                                },
+                                {
+                                    title: 'Liabilities',
+                                    include: false
+                                },
+                                {
+                                    title: 'Income',
+                                    include: false
+                                }
+                            ]
+                        }
+                    />
 
-                <CallAction contactUs={true} callText='Ready to start?' />
+                    <PlansCard
+                        title='Premium Plan'
+                        description='Get tailored solutions, volume pricing, and dedicated support for your team'
+                        price='$499.00'
+                        benefits={
+                            [
+                                {
+                                    title: 'Transaction',
+                                    include: true
+                                },
+                                {
+                                    title: 'Auth',
+                                    include: true
+                                },
+                                {
+                                    title: 'Identity',
+                                    include: true
+                                },
+                                {
+                                    title: 'Investments',
+                                    include: true
+                                },
+                                {
+                                    title: 'Assets',
+                                    include: true
+                                },
+                                {
+                                    title: 'Liabilities',
+                                    include: true
+                                },
+                                {
+                                    title: 'Income',
+                                    include: true
+                                }
+                            ]
+                        }
+                    />
+
+                </div>
+                <CallAction contactUs={false} callText='Ready to start?' />
             </div>
         </main>
     )
