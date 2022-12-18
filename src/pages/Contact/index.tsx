057a9c62e9bf3d2d8f06401893a75b264d391093
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-19 00:11:42 +0900

    feat(contact): add responsiveness

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -4,21 +4,24 @@ import ClientGallery from "../../components/ClientGallery";
 import FormQuestion from "../../components/FormQuestion";
 import Title from "../../components/Title";
 import Style from "../styles/Pages.module.scss";
+import StyleContact from "./Contact.module.scss";
 
 const Contact = () => {
     return (
         <main className={Style["main__background--generic"]}>
-            <div className={Style.gallery}>
-                <div className={classNames(Style.gallery, Style.gallery__secondary)}>
-                    <Title
-                        type="main"
-                        textColor="blue"
-                    >{`Submit a help request and we’ll get in touch shortly.`}</Title>
-                    <FormQuestion />
-                    <Title type="section" textColor="blue">Join the thousands of innovators already building with us</Title>
-                    <ClientGallery backgroundTheme="light" />
-                    <CallAction callText="Ready to start?" />
+            <div className={classNames(Style.gallery, Style.gallery__secondary, StyleContact.main__title)}>
+                        <Title
+                            type="main"
+                            textColor="blue"
+                        >{`Submit a help request and we’ll get in touch shortly.`}</Title>
+                <div className={classNames(StyleContact.form__client)}>
+                        <FormQuestion />
+                    <div>
+                        <Title type="section" textColor="blue">Join the thousands of innovators already building with us</Title>
+                        <ClientGallery backgroundTheme="light" />
+                    </div>
                 </div>
+                <CallAction callText="Ready to start?" />
             </div>
         </main>
     );
